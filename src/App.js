478c6ec960b7b01c9f2dca6b269091f753b55083
NoellyCosta <noellysallazar@gmail.com>
2021-03-19 15:37:33 -0300

    add s1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Cart from './Components/Cart/Cart'
+
+
 
 function App() {
   return (
--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -14,11 +14,11 @@ const Cart1 = styled.div`
     render() {
 
         return (
-            <div className = "cart1">
+            <div className = "Cart1">
                 <h3>Carrinho:</h3>
                 <button onClick={this.adicio}>Remover</button>
                <CartImagem>
-               <img height='15px' width='15px' src={'https://stock.adobe.com/br/images/id/68512486?as_campaign=Flaticon&as_content=api&as_audience=idp&tduid=b54b541d4c7ca23ff738219c67f35136&as_channel=affiliate&as_campclass=redirect&as_source=arvato'} />
+               <img height='15px' width='15px' src={} />
                </CartImagem>
 
             </div>
